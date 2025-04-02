c482e503761cee116465ed48466b7e5b5fcf4650
Y108 <57135720+Y108@users.noreply.github.com>
2025-04-03 01:16:39 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='theta',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='GPL-3.0',
+    description='A collection of sequences and sequence operations',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+    url='https://github.com/Y108/theta',
+    author='Y108',
+    author_email='',
+    python_requires='>=3.8',
+)
