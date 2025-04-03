32aa3d474f6a8c825cb61939da7fd034c7080ece
Y108 <57135720+Y108@users.noreply.github.com>
2025-04-04 01:59:52 +0200

    updates version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='theta',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='GPL-3.0',
     description='A collection of sequences and sequence operations',
