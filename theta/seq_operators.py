e5bdfdcdc6a3d3a0a02555daca01791d20042f19
Y108 <57135720+Y108@users.noreply.github.com>
2025-04-04 01:57:18 +0200

    Functions change
    Assigned frequently used constants to variables in seq_assign

--- theta/seq_operators.py
+++ theta/seq_operators.py
@@ -2,10 +2,15 @@
 
 def seq_assign(A, B): 
         
+        X = len(A)
+        Y = len(B)
+
+        abs_diff = abs(Y - X)
+
         def main(A,B):
 
             S = {i:[] for i in list(set(A))[::-1]}
-            for k in range(len(B)):
+            for k in range(Y):
                 for i in set(A):
                     if A[k] == i:
                         S[i].append(B[k])
@@ -18,28 +23,29 @@ def seq_assign(A, B):
             if n == 2: print('\033[91mWarning: Both inputs of \033[93madd_lst\033[91m have to contain at least \033[93m 1 \033[91m element!\033[0m')
 
         if sorted(list(set(A))) == sorted(A): #check if the output equals the 1-1 assignment of the two sequences
+            
 
-            if len(A) == len(B):
+            if X == Y:
                 print(f'I:{A}\nII:{B}')
             
-            if len(A) > len(B):
-                print(f'I:{A[:-abs(len(B)-len(A))]}\nII:{B}')
+            if X > Y:
+                print(f'I:{A[:-abs_diff]}\nII:{B}')
 
-            if len(A) < len(B):
-                print(f'I:{A}\nII:{B[:-abs(len(B)-len(A))]}')
+            if X < Y:
+                print(f'I:{A}\nII:{B[:-abs_diff]}')
         
         elif not(isinstance(A, list) and isinstance(B, list)):
             errors(2)
         
-        elif not(len(A) > 0 and len(B) > 0):
+        elif not(X > 0 and Y > 0):
             errors(2)
 
         else:
-                if len(A)>=len(B):
+                if X>=Y:
                     main(A,B)
 
-                elif len(A)<len(B):
-                    B2 = B[:-abs(len(B)-len(A))]
+                elif X<Y:
+                    B2 = B[:-abs_diff]
                     main(A,B2)
 
                 else:
