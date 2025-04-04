4c2be49223db3ea400a854f72e2b99a8ab334973
Y108 <57135720+Y108@users.noreply.github.com>
2025-04-04 23:08:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     license='GPL-3.0',
     description='A collection of sequences and sequence operations',
     long_description=open('README.txt').read(),
-    install_requires=[os],
+    install_requires=[],
     url='https://github.com/Y108/theta',
     author='Y108',
     author_email='',
