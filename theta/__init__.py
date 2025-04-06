ae36c9772db3c1baec0caef3dfa6a30778b1f63d
Y108 <57135720+Y108@users.noreply.github.com>
2025-04-07 00:25:51 +0200

    Update __init__.py

--- theta/__init__.py
+++ theta/__init__.py
@@ -1,1 +1,4 @@
-__all__ = ['n_thue_morse', 'seq_operators']
+#__all__ = ['n_thue_morse', 'seq_operators']
+from .sequence_generators import *
+from .sequence_operators import *
+from .sequence_functions import *
