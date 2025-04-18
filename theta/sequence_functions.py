c9005642571f78dade9aa714c97b3949819930a4
Y108 <57135720+Y108@users.noreply.github.com>
2025-04-18 23:35:56 +0200

    Added function

--- theta/sequence_functions.py
+++ theta/sequence_functions.py
@@ -1,4 +1,4 @@
-def seq_assign(A, B): 
+def seq_seq_index(A, B): 
         
         X = len(A)
         Y = len(B)
@@ -48,3 +48,29 @@ def seq_assign(A, B):
 
                 else:
                     return None
+
+def simple_seq_index(A, bound=None):
+    
+    if not isinstance(A, (list, str)) or (not isinstance(bound, int) and bound is not None):
+
+        raise TypeError("Input has to be a list or string, and bound has to be an integer or None")
+
+    elif bound == None:
+        Zip = [[x,y] for x,y in zip(A,range(1,len(A)+1))]
+        S = {i:[] for i in list(sorted(set(A)))[::-1]}
+        for k in range(len(Zip)):
+            for j in S:
+                if Zip[k][0] == j:
+                    S[j].append(Zip[k][1])
+        for key in S:
+            print(f"p({key}): {S[key]}")
+    
+    else:
+        Zip = [[x,y] for x,y in zip(A,range(1,len(A)+1))]
+        S = {i:[] for i in list(set(A))[::-1]}
+        for k in range(min(bound, len(Zip))):
+            for j in S:
+                if Zip[k][0] == j:
+                    S[j].append(Zip[k][1])
+        for key in S:
+            print(f"p({key}): {S[key]}")
