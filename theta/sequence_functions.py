2a778802f5c0eb55d75498b925a486d67a63ae66
Y108 <57135720+Y108@users.noreply.github.com>
2025-04-30 14:20:28 +0200

    Update sequence_functions.py

--- theta/sequence_functions.py
+++ theta/sequence_functions.py
@@ -107,3 +107,27 @@ def seq_index_len_list(A, bound=None):
             templ.append(len(S[key]))
 
         return templ
+
+def peak(A):
+    return [A[i] for i in range(len(A)-1) if A[i] > A[i+1]]
+
+def seq_index_peak(A, bound=None):
+    if not isinstance(A, (list, str)) or (not isinstance(bound, int) and bound is not None):
+        raise TypeError("Input has to be a list or string, and bound has to be an integer or None")
+
+    Zip = [[x, y] for x, y in zip(A, range(1, len(A) + 1))]
+    if bound is None:
+        data = Zip
+    else:
+        data = Zip[:min(bound, len(Zip))]
+
+    S_keys = list(sorted(set(A))) if bound is None else list(set(A))[::-1]
+    S = {i: [] for i in S_keys}
+    
+    for x, y in data:
+        if x in S:
+            S[x].append(y)
+
+    templ = [len(S[key]) for key in S_keys]
+
+    return [S_keys[i] for i in range(len(templ) - 1) if templ[i] > templ[i + 1]]
