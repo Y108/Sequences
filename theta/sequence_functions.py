5ac0eccef9f814cfd0a405cea1042ea3f1550b90
Y108 <57135720+Y108@users.noreply.github.com>
2025-04-30 13:55:28 +0200

    Update sequence_functions.py

--- theta/sequence_functions.py
+++ theta/sequence_functions.py
@@ -75,3 +75,35 @@ def simple_seq_index_len(A, bound=None):
                     S[j].append(Zip[k][1])
         for key in S:
             print(f"p({key}): {len(S[key])}")
+
+def seq_index_len_list(A, bound=None):
+    
+    templ = []
+
+    if not isinstance(A, (list, str)) or (not isinstance(bound, int) and bound is not None):
+
+        raise TypeError("Input has to be a list or string, and bound has to be an integer or None")
+
+    elif bound == None:
+        Zip = [[x,y] for x,y in zip(A,range(1,len(A)+1))]
+        S = {i:[] for i in list(sorted(set(A)))[::-1]}
+        for k in range(len(Zip)):
+            for j in S:
+                if Zip[k][0] == j:
+                    S[j].append(Zip[k][1])
+        for key in S:
+            templ.append(len(S[key]))
+
+        return templ
+    
+    else:
+        Zip = [[x,y] for x,y in zip(A,range(1,len(A)+1))]
+        S = {i:[] for i in list(set(A))[::-1]}
+        for k in range(min(bound, len(Zip))):
+            for j in S:
+                if Zip[k][0] == j:
+                    S[j].append(Zip[k][1])
+        for key in S:
+            templ.append(len(S[key]))
+
+        return templ
