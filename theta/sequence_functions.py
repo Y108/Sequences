91259648b816f0af5e9084fc92f04bf636e37a57
Y108 <57135720+Y108@users.noreply.github.com>
2025-04-30 13:50:22 +0200

    Update sequence_functions.py

--- theta/sequence_functions.py
+++ theta/sequence_functions.py
@@ -24,6 +24,32 @@ def seq_seq_index(A, B, bound=None):
         for key in S:
             print(f"p({key}): {S[key]}")
 
+def simple_seq_index_len(A, bound=None):
+    
+    if not isinstance(A, (list, str)) or (not isinstance(bound, int) and bound is not None):
+
+        raise TypeError("Input has to be a list or string, and bound has to be an integer or None")
+
+    elif bound == None:
+        Zip = [[x,y] for x,y in zip(A,range(1,len(A)+1))]
+        S = {i:[] for i in list(sorted(set(A)))[::-1]}
+        for k in range(len(Zip)):
+            for j in S:
+                if Zip[k][0] == j:
+                    S[j].append(Zip[k][1])
+        for key in S:
+            print(f"p({key}): {len(S[key])}")
+    
+    else:
+        Zip = [[x,y] for x,y in zip(A,range(1,len(A)+1))]
+        S = {i:[] for i in list(set(A))[::-1]}
+        for k in range(min(bound, len(Zip))):
+            for j in S:
+                if Zip[k][0] == j:
+                    S[j].append(Zip[k][1])
+        for key in S:
+            print(f"p({key}): {len(S[key])}")
+
 def simple_seq_index(A, bound=None):
     
     if not isinstance(A, (list, str)) or (not isinstance(bound, int) and bound is not None):
