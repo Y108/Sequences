5adbb6d41d6117de3aa267a9166d8b35b515d487
Y108 <57135720+Y108@users.noreply.github.com>
2025-06-23 22:26:43 +0200

    Update sequence_generators.cpp
    Fixed Bin_Thue_Morse and added Index

--- theta/cpp/sequence_generators.cpp
+++ theta/cpp/sequence_generators.cpp
@@ -6,16 +6,16 @@ std::vector<int> FracDel(const int j) {
     for (int i = 0; i <= j; i++) {
         int G = 3;
         N.clear();
-            for (int s : S) {
-                N.push_back(s);
-                N.push_back(s);
-            }
+        for (int s : S) {
+            N.push_back(s);
+            N.push_back(s);
+        }
         S.clear();
         S = {3};
-            for (int n : N) {
-                G+=n;
-                S.push_back(G%4);
-            }
+        for (int n : N) {
+            G+=n;
+            S.push_back(G%4);
+        }
     }
     return S;
 }
@@ -27,16 +27,25 @@ std::vector<int> Collatz(const int n) {
     while (A.back() != 1) {
         A.push_back((A.back()%2==0)?A.back()/2:(3*A.back()+1));
     }
-        return A;
+    return A;
 }
 
 std::vector<int> Bin_Thue_Morse(int n) {
     std::vector<int> Thue = {0};
-    for (int i = -1; i <= n; i++ ) {
-        for (int k : Thue) {
-            std::cout << Thue.back() << std::endl;
-            Thue.push_back(k == 1 ? 0 : 1);
+    while (0<=n){
+        int size = Thue.size();
+        for (int i = 0; i<size; i++) {
+            Thue.push_back(!Thue[i]);
         }
+        --n;
     }
     return Thue;
+}
+
+std::vector<int> Index(int n) {
+    std::vector<int> Ind;
+    for (int i = 1; i<=n; i++) {
+        Ind.push_back(i);
+    }
+    return Ind;
 }
